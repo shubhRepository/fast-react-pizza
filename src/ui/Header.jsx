7eb3986d2be5f6613135f30a3299e19eeaac4c92
Shubham Maheshwari <academicShubh@gmail.com>
2024-09-26 15:26:32 +0530

    build app layout skeleton

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Shubham</p>
+    </div>
+  );
+}
+
+export default Header;
