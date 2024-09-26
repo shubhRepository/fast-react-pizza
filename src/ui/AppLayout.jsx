c2dd2419741defa74e17a20551c48fd2ee6eb9f5
Shubham Maheshwari <academicShubh@gmail.com>
2024-09-26 17:04:48 +0530

    add loading when fetching data

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
+import { Outlet, useNavigation } from "react-router";
+
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading" ? true : false;
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
