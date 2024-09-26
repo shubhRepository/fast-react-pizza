40b24b5463d83264ca26e85a625dc7a4c0ecbc09
Shubham Maheshwari <academicShubh@gmail.com>
2024-09-26 17:19:50 +0530

    add error handling component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import Error from "./ui/Error";
+import { loader as menuLoader } from "./features/menu/loader";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -20,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
