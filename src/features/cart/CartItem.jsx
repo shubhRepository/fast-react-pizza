d0af73bbb667adbfb3dcace1230f45495f3ef352
Shubham Maheshwari <academicShubh@gmail.com>
2024-09-28 22:59:26 +0530

    style cart page

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,15 +1,27 @@
+import PropTypes from "prop-types";
 import { formatCurrency } from "../../utils/helpers";
+import Button from "../../ui/Button";
+
+CartItem.propTypes = {
+  item: PropTypes.shape({
+    pizzaId: PropTypes.number,
+    name: PropTypes.string,
+    quantity: PropTypes.number,
+    totalPrice: PropTypes.number,
+  }),
+};
 
 function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+  const { name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
+      <div className="flex items-center justify-between sm:gap-6">
         <p>{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
